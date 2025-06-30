ea88fb382908951feaee4e81f980eafb3bb1e41f
Saka1r <sakairlinux@gmail.com>
2025-06-30 22:44:05 +0300

    Basic structure Lexer class, add structure new methods

--- src/lexer.cpp
+++ src/lexer.cpp
@@ -19,7 +19,37 @@ struct Token {
 };
 
 class Lexer {
+    private:
+        std::string input;
+        size_t currentIndex; 	
     
+    public: 
+        
+        Lexer(const std::string& str) : input(str), currentIndex(0) {}
+
+        void SkipWhiteSpace(std::string &input)
+        {
+            size_t i = 0;
+            while (i < input.size())
+            {
+                if(!isspace(input[1]))
+                        {break;}
+                i++;
+            }
+            input.erase(0, i); // Удаление всех пробелов в начале строки
+        }
+
+        bool IsAtEnd() const 
+        {
+            return currentIndex >= input.size();
+        }
+
+        Token GetNextToken();
+
+        void ReadIndetifier();
+
+        void ReadNumber();
+
 };
 
 int main(void)
