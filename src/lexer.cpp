f8f5fb32a82daa5d12d41ca6f39fff524c4b4a36
Saka1r <sakairlinux@gmail.com>
2025-06-30 23:23:48 +0300

    Lexer Update

--- src/lexer.cpp
+++ src/lexer.cpp
@@ -22,37 +22,87 @@ class Lexer {
     private:
         std::string input;
         size_t currentIndex; 	
-    
+        std::string numberValue;
+        std::string identifierValue;
     public: 
-        
         Lexer(const std::string& str) : input(str), currentIndex(0) {}
 
-        void SkipWhiteSpace(std::string &input)
-        {
-            size_t i = 0;
-            while (i < input.size())
-            {
-                if(!isspace(input[1]))
-                        {break;}
-                i++;
-            }
-            input.erase(0, i); // Удаление всех пробелов в начале строки
-        }
+        void SkipWhiteSpace(){
+
+            while(currentIndex < input.size() && isspace(input[currentIndex])){
+                currentIndex++;}
+        } 
 
         bool IsAtEnd() const 
         {
             return currentIndex >= input.size();
         }
 
-        Token GetNextToken();
+        void ReadIdetifier(){
+            identifierValue = "";
+            while (currentIndex < input.size() && (isalnum(input[currentIndex]) || input[currentIndex] == '_')) {
+                identifierValue += input[currentIndex];
+                currentIndex++;
+            }
+        }
+        void ReadNumber()
+        {
+            numberValue = "";
+            while(currentIndex < input.size() && isdigit(input[currentIndex])){
+                numberValue += input[currentIndex];
+                currentIndex++;
+            }
+        }
+        
+        Token GetNextToken()
+        {
+            SkipWhiteSpace();
 
-        void ReadIndetifier();
+            if(IsAtEnd()){
+                return {TokenType::End, ""};}
 
-        void ReadNumber();
+            if (isalpha(input[currentIndex]) || input[currentIndex] == '_') {
+                ReadIdetifier();
+                return {TokenType::Identifier, identifierValue};
+            }
 
+            if(isdigit(input[currentIndex])){
+                ReadNumber();
+                return {TokenType::Number, numberValue};}
+
+            return {TokenType::Invalid, std::string(1, input[currentIndex])};
+        }
 };
 
 int main(void)
 {
+    std::string inputString = "123 abc _var 456";
+    Lexer lexer(inputString);
+
+    while (true) {
+        Token token = lexer.GetNextToken();
+        if (token.type == TokenType::End) {
+            break;
+        }
+        // Вывод токенов
+        switch (token.type) {
+            case TokenType::Command:
+                std::cout << "Token: Command Value: " << token.value << std::endl;
+                break;
+            case TokenType::Identifier:
+                std::cout << "Token: Identifier Value: " << token.value << std::endl;
+                break;
+            case TokenType::Number:
+                std::cout << "Token: Number Value: " << token.value << std::endl;
+                break;
+            case TokenType::Invalid:
+                std::cout << "Token: Invalid Value: " << token.value << std::endl;
+                break;
+            case TokenType::End:
+                std::cout << "Token: End Value: " << token.value << std::endl;
+                break;
+        }
+    }
+
     return 0;
 }
